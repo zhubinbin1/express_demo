72442aa099ea413898ad1400c2321524edd037c7
binbin.zhu <binbin.zhu@opay-inc.com>
2020-03-29 07:19:13 +0800

    处理登陆

--- routes/user.js
+++ routes/user.js
@@ -22,13 +22,29 @@ router.post("/signup",function(req,res){
     //
     // res.render("user/signup",{title:"注册"})
 });
+router.get("/signin",function(req,res){
+    res.render("user/signin",{title:"登陆"})
+});
+router.post("/signin",function(req,res){
+    let user =req.body// 请求体对象,username pw email
+    console.log(JSON.stringify(user))
+    User.findOne(user,function(err,doc){
+        // console.log("=="+dec)
+        console.log(JSON.stringify(doc))
+        if(err){
+            res.redirect('back');
+        }else if(doc){
+            res.redirect('/');
+        }else{
+            res.redirect('back');
+        }
+    })
+    // res.render("user/signup",{title:"注册"})
+});
 router.get("/signout",function(req,res){
     res.send("用户退出")
     // res.render("user/s",{title:"用户退出"})
 });
-router.get("/signin",function(req,res){
-    // res.send("用户登陆")
-    res.render("user/signin",{title:"登陆"})
-});
+
 //module 模块  model 模型
 module.exports = router;
